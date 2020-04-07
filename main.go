4dc59f7709e10c1da4087fad31ec94efd05d0746
Sourav Kumar <sourav.kumar@traveltriangle.com>
2020-04-07 18:22:28 +0530

    fds

--- main.go
+++ main.go
@@ -1,45 +1,10 @@
 package main
 
 import (
-  "github.com/gorilla/mux"
-  "net/http"
-  "strconv"
-  "github.com/souravTT/testing_go/app/controller"
+  "github.com/souravTT/testing_go/server"
 )
 
-func groupPaths(router *mux.Router){
-	router.HandleFunc("/groups", controller.Group.Index).Methods(http.MethodGet)
-  router.HandleFunc("/group/new", controller.Group.New).Methods(http.MethodGet)
-	router.HandleFunc("/group/{id}", controller.Group.Show).Methods(http.MethodGet)
-	router.HandleFunc("/group", controller.Group.Create).Methods(http.MethodPost)
-	router.HandleFunc("/group/{id}", controller.Group.Update).Methods(http.MethodPut)
-	router.HandleFunc("/group/{id}", controller.Group.Delete).Methods(http.MethodDelete)
-	router.HandleFunc("/group/{id}/assign_roles", controller.Group.AssignRoles).Methods(http.MethodPost)
-}
-
 func main(){
-  Server()
+  server.Server()
 }
 
-func handleRequests() {
-  router := mux.NewRouter().StrictSlash(true)
-
-  groupPaths(router)
-
-  host := "127.0.0.1"
-  port := 3000
-
-  address := ""
-  if host != ""{
-    address += host
-  }
-  address += ":"
-  if port != 0 {
-    address += strconv.Itoa(port)
-  }
-  _ = http.ListenAndServe(address, router)
-}
-
-func Server() {
-  handleRequests()
-}
---
+++ server/server.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+  "github.com/gorilla/mux"
+  "net/http"
+  "strconv"
+  "github.com/souravTT/testing_go/app/controller"
+)
+
+func groupPaths(router *mux.Router){
+  router.HandleFunc("/groups", controller.Group.Index).Methods(http.MethodGet)
+  router.HandleFunc("/group/new", controller.Group.New).Methods(http.MethodGet)
+  router.HandleFunc("/group/{id}", controller.Group.Show).Methods(http.MethodGet)
+  router.HandleFunc("/group", controller.Group.Create).Methods(http.MethodPost)
+  router.HandleFunc("/group/{id}", controller.Group.Update).Methods(http.MethodPut)
+  router.HandleFunc("/group/{id}", controller.Group.Delete).Methods(http.MethodDelete)
+  router.HandleFunc("/group/{id}/assign_roles", controller.Group.AssignRoles).Methods(http.MethodPost)
+}
+
+func handleRequests() {
+  router := mux.NewRouter().StrictSlash(true)
+
+  groupPaths(router)
+
+  host := "127.0.0.1"
+  port := 3000
+
+  address := ""
+  if host != ""{
+    address += host
+  }
+  address += ":"
+  if port != 0 {
+    address += strconv.Itoa(port)
+  }
+  _ = http.ListenAndServe(address, router)
+}
+
+func Server() {
+  handleRequests()
+}
